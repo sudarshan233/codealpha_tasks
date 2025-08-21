1b0a6f447cfef17ad2fb4f92069acd5a6ad03529
sudarshan233 <nsridharsudarshan@gmail.com>
2025-08-21 20:54:29 +0530

    Ready for CI/CD

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import router from './routes/taskRoutes.js';
 import {connectDB} from "./config/database.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173",
+        }
+    ))
+}
 
 connectDB();
-app.use(cors());
-app.use(express.json());
 app.use('/api/tasks', router);
+
+if(process.env.NODE_ENV === 'production') {
+    const frontendPath = path.join(__dirname, '../frontend/dist');
+    app.use(express.static(frontendPath));
+    app.get(/^\/(?!api).*/, (req, res) => {
+        res.sendFile(path.join(frontendPath, 'index.html'));
+    });
+}
 
 app.listen(PORT, (error) => {
     if(error) console.error(error);
--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+const BASE_URl = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api/tasks' : '/api/tasks';
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/api/tasks'
+    baseURL: BASE_URl,
 })
