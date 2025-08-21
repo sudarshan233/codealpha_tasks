335f9c9f840059e0a262fedb84c9f308571bed70
sudarshan233 <nsridharsudarshan@gmail.com>
2025-08-21 21:19:08 +0530

    Ready for CI/CD

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URl = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api/tasks' : '/api/tasks';
+const BASE_URl = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api/tasks' : '/api';
 export const api = axios.create({
     baseURL: BASE_URl,
 })
