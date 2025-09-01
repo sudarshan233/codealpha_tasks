c234fa4927cdecaa4e88abc8a7431e909f4f1e30
sudarshan233 <nsridharsudarshan@gmail.com>
2025-09-01 18:09:23 +0530

    Ready for deployment: Trial-1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import router from './routes/taskRoutes.js';
 import {connectDB} from "./config/database.js";
@@ -8,12 +9,21 @@ import {connectDB} from "./config/database.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', router);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+
+}
 app.listen(PORT, (error) => {
     if(error) console.error(error);
     console.log(`Server is running on port ${PORT}`);
